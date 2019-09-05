45f932c00b3ed1ff94fc342535d9f7b93f15cc6b
teemoshroom321 <dibas@datareporting.onmicrosoft.com>
2019-09-05 13:29:58 -0400

    second message including app prefix to sentence

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 
 function Hi() {
-  return <p>Hello world from react.</p>;
+  return <p>Hello world from react app.</p>;
 }
 
 render(<Hi />, document.getElementById("app"));
